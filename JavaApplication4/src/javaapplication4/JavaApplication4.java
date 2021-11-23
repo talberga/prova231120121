567c67a5bfc02ace03372f10ad0ef2904bb7be94
teresa.alberga <teresa.alberga@AULA75-PC08.itib.local>
2021-11-23 15:24:14 +0100

    preparazione test

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -3,6 +3,7 @@ package javaapplication4;
 public class JavaApplication4 {
 
     public static void main(String[] args) {
-        System.out.println("ciao mondo!!");
+        int somma = 0;
+        System.out.println("somma = " + somma);
     }
 }
