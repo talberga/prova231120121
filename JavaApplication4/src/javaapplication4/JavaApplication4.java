8b10cc217a4875d5870e6df91ab127a5af1ffa31
anamaria.ilisei <anamaria.ilisei@AULA75-PC09.itib.local>
2021-11-23 15:37:32 +0100

    ho fatto la somma!

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -4,6 +4,7 @@ public class JavaApplication4 {
 
     public static void main(String[] args) {
         int somma = 0;
+        somma = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);       
         System.out.println("somma = " + somma);
     }
 }
